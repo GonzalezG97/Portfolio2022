6e63fccf6be215012afe83a1f0a3be45113677b4
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2022-02-18 13:55:49 -0600

    Added body to About ME

--- my-app/src/components/About Me/AboutMe.jsx
+++ my-app/src/components/About Me/AboutMe.jsx
@@ -10,6 +10,14 @@ function AboutMe() {
             <Typography variant="h3" component="h1">
                 About Me
             </Typography>
+            <Typography variant="body1" component="p">
+                Hello and welcome to my website! I am new to web development and created
+                this to show off my projects and new skills. I worked in customer
+                support for many big names in tech. I hope to use my ability to
+                understand how the average customer thinks to the development side.
+                Armed with this knowledge I plan to build applications and websites with
+                the most common user issues in mind to help minimize user error.
+            </Typography>
             </Container>                
         </div>
     );
