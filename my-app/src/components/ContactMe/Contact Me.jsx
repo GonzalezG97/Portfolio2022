3003b7fd46866646df2f6fba92a77d16790f42a7
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2022-02-22 19:33:24 -0600

    Added grid with links

--- my-app/src/components/ContactMe/Contact Me.jsx
+++ my-app/src/components/ContactMe/Contact Me.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import Typography from "@mui/material/Typography"
+import Typography from "@mui/material/Typography";
 import Container from '@mui/material/Container';
+import Grid from "@mui/material/Grid";
+
 
 
 function ContactMe() {
@@ -10,7 +12,30 @@ function ContactMe() {
             <Typography variant="h3" component="h1">
                 Contact Me
             </Typography>
-            </Container>                
+            </Container> 
+            <Container>
+            <Grid item sm={3}>
+            <div>
+              <Typography variant="h5" component="p">
+                gustavoshadow13@gmail.com
+            </Typography>
+            </div>
+            <div>
+              <a href="https://github.com/GonzalezG97">
+                <Typography variant="h5" component="p">
+                   Github
+                </Typography>
+              </a>
+            </div>
+            <div>
+              <a href="https://www.linkedin.com/in/gustavo-gonzalez-00362215b/">
+                <Typography variant="h5" component="p">
+                 Linkedin
+                </Typography>
+              </a>
+            </div>
+          </Grid>
+            </Container>               
         </div>
     );
 }
