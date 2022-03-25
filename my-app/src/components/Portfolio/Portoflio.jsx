d0a4483fa7dea610738a84971bd83b5531dc544a
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2022-03-25 12:54:06 -0500

    Added Grid to Portfolio

--- my-app/src/components/Portfolio/Portoflio.jsx
+++ my-app/src/components/Portfolio/Portoflio.jsx
@@ -9,8 +9,31 @@ import Grid from '@mui/material/Grid';
 
 export default function Portoflio() {
   return (
-      <Grid>
+      <Grid container spacing={2}>
           <Grid item>
+        <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image="/static/images/cards/contemplative-reptile.jpg"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+        </Card>
+            </Grid>
+            <Grid item>
         <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         component="img"
