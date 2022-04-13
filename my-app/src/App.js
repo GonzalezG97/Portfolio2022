0c035a723b2ed30947de1b0c8ab4f321641252fa
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2022-04-12 20:27:43 -0500

    Added resume file on mando click

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,5 @@
 import "./App.css";
-import AboutMe from "../src/components/About Me/AboutMe";
-import ContactMe from "../src/components/ContactMe/ContactMe";
-import Portfolio from "../src/components/Portfolio/Portoflio";
 import Navbar from "./components/Navbar/Navbar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
--- my-app/src/components/Navbar/Navbar.jsx
+++ my-app/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,12 @@
 import { React, useState, useEffect } from "react";
 import AppBar from "@mui/material/AppBar";
 import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
 import Toolbar from "@mui/material/Toolbar";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
-import IconButton from "@mui/material/IconButton";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMandalorian } from "@fortawesome/free-brands-svg-icons";
-import { faFileUser } from "@fortawesome/free-solid-svg-icons";
 import AboutMe from "../About Me/AboutMe";
 import ContactMe from "../ContactMe/ContactMe";
 import Portfolio from "../Portfolio/Portoflio";
@@ -36,7 +26,11 @@ function Navbar() {
     <>
       <AppBar position="sticky" sx={{ background: "black" }}>
         <Toolbar>
-          <Button color="secondary">
+          <Button color="secondary"
+          onClick={() => {
+            window.open('https://drive.google.com/file/d/18hsSEL0ZZk5uXMOLvnjc80IdGhsqXzU9/view?usp=sharing')
+          }}
+          >
             <Typography variant="h3">
               <FontAwesomeIcon icon={faMandalorian} />
             </Typography>
