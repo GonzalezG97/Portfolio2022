197eda4455c6b05e514b615dfa792d04e13e73ea
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2022-04-25 21:55:56 -0500

    Added copyright and font awesome

--- my-app/src/components/Footer/Footer.jsx
+++ my-app/src/components/Footer/Footer.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopyright} from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
   return (
@@ -9,7 +11,12 @@ function Footer() {
         <Grid>
             <Grid item>
                 <Typography variant="h6" component="h1">
-                Built with React and MaterialUI
+                Built with ReactJs and MaterialUI
+                </Typography>
+            </Grid>
+            <Grid item>
+                <Typography variant="subtitle2" component="h1">
+                <FontAwesomeIcon icon={faCopyright}/> 2022
                 </Typography>
             </Grid>
         </Grid>
