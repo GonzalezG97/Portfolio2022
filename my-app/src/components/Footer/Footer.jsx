96807999f66d48dd876248d6dd07b1b178dcfba9
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2022-04-25 22:01:50 -0500

    Added linkedIn to footer

--- my-app/src/components/Footer/Footer.jsx
+++ my-app/src/components/Footer/Footer.jsx
@@ -1,27 +1,36 @@
-import React from 'react';
+import React from "react";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCopyright} from "@fortawesome/free-solid-svg-icons";
+import { faCopyright } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
   return (
     <Container>
-        <Grid>
-            <Grid item>
-                <Typography variant="h6" component="h1">
-                Built with ReactJs and MaterialUI
-                </Typography>
-            </Grid>
-            <Grid item>
-                <Typography variant="subtitle2" component="h1">
-                <FontAwesomeIcon icon={faCopyright}/> 2022
-                </Typography>
-            </Grid>
+      <Grid>
+        <Grid item>
+          <Typography variant="h6" component="h1">
+            Built with ReactJs and MaterialUI
+          </Typography>
         </Grid>
+        <Grid item>
+          <Typography variant="subtitle2" component="h2">
+            <FontAwesomeIcon icon={faCopyright} /> 2022
+          </Typography>
+        </Grid>
+        <Grid>
+          <Grid item>
+              <Button>
+                <FontAwesomeIcon icon={faLinkedin} />
+              </Button>
+          </Grid>
+        </Grid>
+      </Grid>
     </Container>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
